18677553fcc459c191494780817ebb3d1f7b070b
richkode <rtweneboanah@gmail.com>
2022-03-11 12:22:02 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print all possible combinations of two digits
+ * Return: 0 on success code
+ */
+int main(void)
+{
+	int i;
+	int j = 49;
+	for (i = 48; i <= 56; ;)
+	{
+		if (j == 57)
+		{
+			i++;
+			j = 49;
+		}
+		putchar(i);
+		putchar(j);
+		if (i <= 56)
+		{
+			putchar(',');
+			putchar(' ');
+			j++;
+		}
+		putchar('\n');
+	}
+}
