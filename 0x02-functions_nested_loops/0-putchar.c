884e0ef0eae0b738f2cf367cabf2c2a95df80af8
richkode <rtweneboanah@gmail.com>
2022-03-15 14:00:42 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - prints _putchar
+ * Return: 0 on success code.
+ */
+int main(void)
+{
+	char str[] = "_putchar";
+	int i;
+
+	for (i = 0; i < strlen(str); i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar(str[i]);
+	return (0);
+}
